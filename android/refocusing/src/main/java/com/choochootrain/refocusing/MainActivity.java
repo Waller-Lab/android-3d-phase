655d548de12c9eb643517f37593fdbe6b03242b6
Hurshal Patel <hpatel516@gmail.com>
2014-03-26 14:29:30 -0700

    add basic image viewer

---
+++ android/refocusing/src/main/java/com/choochootrain/refocusing/MainActivity.java
@@ -0,0 +1,50 @@
+package com.choochootrain.refocusing;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+
+import java.io.InputStream;
+
+
+public class MainActivity extends Activity {
+
+    private ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView = (ImageView) findViewById(R.id.imageView);
+        InputStream is = this.getResources().openRawResource(R.drawable.test);
+        Bitmap bitmap = BitmapFactory.decodeStream(is);
+        imageView.setImageBitmap(bitmap);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
