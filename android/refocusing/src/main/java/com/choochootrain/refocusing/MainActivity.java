c6f506eafa5b8cba136270c3241885b84340fcc7
Hurshal Patel <hpatel516@gmail.com>
2014-04-17 21:58:55 -0800

    refactor OpenCVActivity

--- android/refocusing/src/main/java/com/choochootrain/refocusing/MainActivity.java
+++ android/refocusing/src/main/java/com/choochootrain/refocusing/MainActivity.java
@@ -1,22 +1,16 @@
 package com.choochootrain.refocusing;
 
-import android.app.Activity;
 import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 
+import com.choochootrain.refocusing.opencv.OpenCVActivity;
 import com.choochootrain.refocusing.tasks.ComputeFocusTask;
 
-import org.opencv.android.BaseLoaderCallback;
-import org.opencv.android.LoaderCallbackInterface;
-import org.opencv.android.OpenCVLoader;
-
-
-public class MainActivity extends Activity {
+public class MainActivity extends OpenCVActivity {
     private static final String TAG = "MainActivity";
     private static final String DATASET = "10-1-13";
 
@@ -24,23 +18,6 @@ public class MainActivity extends Activity {
     private Button computeButton;
     private ImageView imageView;
     private ImageUtils imageUtils;
-
-    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
-        @Override
-        public void onManagerConnected(int status) {
-            switch (status) {
-                case LoaderCallbackInterface.SUCCESS: {
-                    Log.i(TAG, "OpenCV loaded successfully");
-
-                    // Load native libraries after(!) OpenCV initialization
-                    postOpenCVLoad();
-                } break;
-                default: {
-                    super.onManagerConnected(status);
-                } break;
-            }
-        }
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,15 +39,10 @@ public class MainActivity extends Activity {
         imageView = (ImageView) findViewById(R.id.imageView);
     }
 
-    private void postOpenCVLoad() {
+    @Override
+    public void postOpenCVLoad() {
         imageUtils = new ImageUtils(this, DATASET);
         computeButton.setEnabled(true);
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_8, this, mLoaderCallback);
     }
 
     public void setImage(Bitmap bmp) {
