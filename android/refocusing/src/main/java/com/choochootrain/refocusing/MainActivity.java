9e52470785e51a783bdb69d0d7bae3b64f099998
Hurshal Patel <hpatel516@gmail.com>
2014-05-07 18:58:18 -0700

    clean up imports

--- android/refocusing/src/main/java/com/choochootrain/refocusing/MainActivity.java
+++ android/refocusing/src/main/java/com/choochootrain/refocusing/MainActivity.java
@@ -7,7 +7,6 @@ import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.Toast;
 
